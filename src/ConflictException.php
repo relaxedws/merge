38abff81c069f951ebaa6dc2342b49bad15d3f3b
Rakesh Verma <rkv160794@gmail.com>
2016-06-17 16:57:32 +0530

    Added tests, Improved readability

--- src/ConflictException.php
+++ src/ConflictException.php
@@ -4,7 +4,8 @@ namespace Relaxed\Merge\ConflictException;
 
 use Exception;
 
-class ConflictException extends Exception{
+class ConflictException extends Exception
+{
     public function __construct($message = "", $code = 0, Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
